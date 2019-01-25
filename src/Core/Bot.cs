e68c07a4294f1edb7355256e1e518c2c2eec7682
Ruben <24549041+RDieleman@users.noreply.github.com>
2019-01-25 16:39:50 +0100

    finished discord setup

---
+++ src/Core/Bot.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Bot
+    {
+        private readonly IDiscord _discord;
+
+        public Bot(IDiscord discord)
+        {
+            _discord = discord;
+        }
+
+        public async Task RunAsync()
+        {
+            await _discord.RunAsync();
+        }
+
+    }
+}

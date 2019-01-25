46b496e7fd43b6f3c68a22f347d07670ff71d8bd
Ruben <24549041+RDieleman@users.noreply.github.com>
2019-01-25 21:17:59 +0100

    Created BotEmbed entity

---
+++ src/Core/Entities/BotColor.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Entities
+{
+    public class BotColor
+    {
+        public byte R { get; }
+        public byte G { get; }
+        public byte B { get; }
+
+        public BotColor(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+    }
+}
---
+++ src/Core/Entities/BotEmbed.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Entities
+{
+    public class BotEmbed
+    {
+        public BotAuthor Author { get; set; }
+        public BotColor Color { get; set; }
+        public string Description { get; set; }
+        public BotFooter Footer { get; set; }
+        public Uri ImageUrl { get; set; }
+        public Uri ThumbnailUrl { get; set; }
+        public string Title { get; set; }
+        public DateTime? Timestamp { get; set; }
+        public Uri Url { get; set; }
+        private readonly List<BotField> _fields = new List<BotField>();
+
+        public IEnumerable<BotField> GetFields()
+            => _fields.AsEnumerable();
+
+        public void RemoveField(BotField field)
+            => _fields.Remove(field);
+
+        public void AddField(BotField field)
+            => _fields.Add(field);
+    }
+
+    public class BotAuthor
+    {
+        public string IconUrl { get; }
+        public string Name { get; }
+        public string Url { get; }
+
+        public BotAuthor(string iconUrl = null, string name = null, string url = null)
+        {
+            IconUrl = iconUrl;
+            Name = name;
+            Url = url;
+        }
+    }
+
+    public class BotField
+    {
+        public string Name { get; }
+        public string Value { get; }
+        public bool Inline { get; set; }
+
+        public BotField(string name, string value, bool inline = false)
+        {
+            Name = name;
+            Value = value;
+            Inline = inline;
+        }
+    }
+
+    public class BotFooter
+    {
+        public string Text { get; }
+        public string IconUrl { get; }
+
+        public BotFooter(string text = null, string iconUrl = null)
+        {
+            Text = text;
+            IconUrl = iconUrl;
+        }
+    }
+}
